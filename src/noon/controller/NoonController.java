165494bfabd89af6078b74962585331f92725302
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-09-07 13:52:39 -0600

    Learned about using a Scanner Object and more DS store stuff because why not?

--- src/noon/controller/NoonController.java
+++ src/noon/controller/NoonController.java
@@ -1,10 +1,23 @@
 package noon.controller;
 
+import java.util.Scanner;
+
 public class NoonController 
 {
 	
 	public void start()
 	{
 		System.out.println("I am an very boring person");
+		
+		testScanner();
+	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner;
+		firstScanner = new Scanner(System.in);
+		System.out.println("What is your favorite poisoned berry?");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh cool, you like " + answer + ".");
 	}
 }
