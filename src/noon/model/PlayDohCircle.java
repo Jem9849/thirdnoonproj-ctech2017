000672301317642709af2a79442c8e612d960c96
Jeremy Saysanasy <jeremysaysanasy@gmail.com>
2017-09-11 12:20:31 -0600

    More constructors for playdoh class

--- src/noon/model/PlayDohCircle.java
+++ src/noon/model/PlayDohCircle.java
@@ -11,4 +11,15 @@ public class PlayDohCircle
 	{
 		this.size = 5;
 	}
+	
+	public PlayDohCircle(int size)
+	{
+		this.size = size;
+	}
+	
+	public String toString()
+	{
+		String desc = "I am a circle, my size is " + size;
+		return desc;
+	}
 }
